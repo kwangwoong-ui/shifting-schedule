a7a0cf590e29bebc3f928fa21759d11da17e3261
kwangwoong-ui <kwangwoong92@gmail.com>
2026-01-14 07:58:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ for s in display_modes:
     reg_booths = full_groups * (s - 1)
     total_booths = reg_booths + rem # 잉여 인원 합산 로직
     
-    label = "2교대(맞교대)" if s == 2 else f"{s}교대"
+    label = "맞교대" if s == 2 else f"{s}교대"
     
     table_data.append({
         "운영 모드": label,
